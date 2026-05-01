75a5f0e30ce6e660770564dacfe9f970b5b6a959
Akshat Bhardwaj <130995326+akshatbh00@users.noreply.github.com>
2026-05-01 13:28:54 +0530

    Add Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    #allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=[
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://hire-flow-kohl.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
